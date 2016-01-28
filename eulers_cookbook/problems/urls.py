1116cd7c80d8773b9c24955f3d56079757035547
Zach Calvert <zachc@darkhorse.com>
2016-01-28 14:44:58 -0800

    InteractiveCallbackView and problem one solved

--- eulers_cookbook/problems/urls.py
+++ eulers_cookbook/problems/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import include, url
 from django.views.generic import TemplateView
-from problems import views
+from problems import utils, views
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view(), name='site_home'),
     url(r'^about/$', TemplateView.as_view(template_name="about.html"), name='about'),
 
-    url(r'^(?P<problem_number>\d+)(\..+)?/$', views.EulerProblemView.as_view(), name='euler_problem'),    
+    url(r'^(?P<problem_number>\d+)(\..+)?/$', views.EulerProblemView.as_view(), 
+    	name='euler_problem'),    
+
+
+	url(r'^1/multiples_of_three_and_five$', views.ProblemOneView.as_view(), 
+		name='multiples_of_three_and_five'),	
 ]
--- eulers_cookbook/problems/views.py
+++ eulers_cookbook/problems/views.py
@@ -1,10 +1,11 @@
 import json
 from datetime import datetime
 
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from problems.models import Problem
@@ -37,20 +38,53 @@ class HomePageView(TemplateView):
 
 
 class EulerProblemView(TemplateView):
-    template_name = "problem.html"
+    template_name = 'problem.html'
 
     def get_context_data(self, *args, **kwargs):
         context = super(EulerProblemView, self).get_context_data(**kwargs)
-        
-        name = "solutions/{}.html".format(kwargs['problem_number'])
-        try:
-            get_template(name)
-        except TemplateDoesNotExist:
-            template_name = "problem.html" 
 
         problem = get_object_or_404(Problem, number=kwargs['problem_number'])    
 
         context['problem'] = problem
 
         return context
+
+
+class InteractiveCallbackView(View):
+
+    def get_current_time(self):
+        now = datetime.now()
+        return now.strftime('%m-%d %H:%M:%S')
+
+    def get(self, request):
+        number = request.GET.get('number', None)    
+
+        # ensure a value is provided
+        if not number:
+            return HttpResponse('Please provide a value.', status=400)    
+
+        # ensure the value is an integer
+        try:
+            number = int(number)
+        except ValueError:
+            return HttpResponse('Please provide an integer value.', status=400)
+
+        value = self.calculate_result(number) 
+
+        content = {
+            'number': number,
+            'value': value,
+            'last_requested': self.get_current_time()
+        }
+
+        return HttpResponse(json.dumps(content))
+
+
+class ProblemOneView(InteractiveCallbackView):
+    """
+    Problem 1: Multiples of Three and Five
+    """
+    def calculate_result(self, number):
+        return sum([i for i in range(number) if i % 3 == 0 or i % 5 == 0])   
+
 
